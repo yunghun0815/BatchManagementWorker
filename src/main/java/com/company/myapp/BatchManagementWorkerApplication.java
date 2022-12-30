74605eb67f9d9215487ab524f03a4406582fa096
정영훈 <yunghun0815@gmail.com>
2022-12-30 16:32:19 +0900

    init

---
+++ src/main/java/com/company/myapp/BatchManagementWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.company.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchManagementWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchManagementWorkerApplication.class, args);
+	}
+
+}
